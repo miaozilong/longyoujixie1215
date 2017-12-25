030dba182e32c8097b0e7ee5e676ad8a0f53afd4
miaozilong <miao.zilong@qq.com>
2017-12-25 10:00:28 +0800

    完成关于我们

--- src/routes/AboutUs/AboutUs.js
+++ src/routes/AboutUs/AboutUs.js
@@ -1,18 +1,19 @@
 import React from 'react'
-import { browserHistory } from 'react-router'
+import {browserHistory} from 'react-router'
 import classes from './AboutUs.css'
-import testPic from './20171223225003.png'
+import AboutSideBar from 'components/AboutSideBar'
+import AboutUsView from 'components/AboutUs'
+
 
 const goBack = (e) => {
-  e.preventDefault()
-  return browserHistory.goBack()
+    e.preventDefault()
+    return browserHistory.goBack()
 }
 
 export const AboutUs = () => (
-  <div>
-    <h4>About us</h4>
-    <img src={testPic} alt="图片"/>
-  </div>
+    <div>
+       <AboutUsView/>
+    </div>
 )
 
 export default AboutUs
