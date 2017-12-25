2d89a50a13ae8209f49265ab93a2e0778f58cdb4
miaozilong <miao.zilong@qq.com>
2017-12-25 10:59:29 +0800

    完成关于我们

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -4,18 +4,18 @@ import AboutSideBar from '../AboutSideBar'
 import AboutMain from '../AboutMain'
 
 export const AboutUs = () => (
-  <div>
-      <div className="container about-content">
-          <div className="row">
-              <div className="col-md-2">
-                  <AboutSideBar/>
-              </div>
-              <div className="col-md-10">
-                  <AboutMain />
-              </div>
-          </div>
-      </div>
-  </div>
+    <div>
+        <div className="container about-content">
+            <div className="row">
+                <div className="col-md-2">
+                    <AboutSideBar/>
+                </div>
+                <div className="col-md-10">
+                    <AboutMain/>
+                </div>
+            </div>
+        </div>
+    </div>
 )
-
 export default AboutUs
+
